60b1537ac3f60b44b752f0daeb7ab9c20059fdc3
Andrey <Sm1le84@mail.ru>
2022-12-29 16:53:43 +0300

    initial commit

---
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -0,0 +1,12 @@
+package ru.netology.service;
+
+public class CashbackHackService {
+    private final int boundary = 1000;
+
+    public int remain(int amount) {
+        return boundary - amount % boundary;
+
+        // 1000 - (900/1000)=   1000-900 = 100
+        // 1000 - ()
+    }
+}
